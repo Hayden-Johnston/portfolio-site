ce3ef5c4b487341848e41f8ece7eb3f6a7b0e71b
Hayden-Johnston <hjohnston.g@gmail.com>
2023-08-23 12:53:22 -0700

    updated image paths

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,8 +5,8 @@ const projects = [
     { project: 'MERN Inventory', tech: 'React, Node.js, Express, MongoDB', link: 'https://github.com/Hayden-Johnston/MERN-inventory', image: 'images/mern-inventory.jpg'},
     { project: 'Historical Stock Analysis', tech: 'Python, Yfinance, BeautifulSoup, Pandas, Plotly', link: 'https://github.com/Hayden-Johnston/IBM-datascience', image: 'images/stock-analysis.jpg'},
     { project: 'Discord Chatbot', tech: 'Python, Discord.py, OpenAI API, Docker', link: 'https://github.com/hgjohn/Discord-GPT-bot', image: 'images/QR8R.jpg'},
-    { project: 'Flask Backend', tech: 'Python, Flask, SQlite', link: 'https://github.com/Hayden-Johnston/Flask-SQLite-backend', image: ''},
-    { project: 'Portfolio Site', tech: 'React, Node.js, MaterialUI', link: 'https://github.com/Hayden-Johnston/portfolio-site', image: ''},
+    { project: 'Flask Backend', tech: 'Python, Flask, SQlite', link: 'https://github.com/Hayden-Johnston/Flask-SQLite-backend', image: 'images/flask-sqlite.jpg'},
+    { project: 'Portfolio Site', tech: 'React, Node.js, MaterialUI, EmailJS', link: 'https://github.com/Hayden-Johnston/portfolio-site', image: 'images/anime-me.jpg'},
     { project: 'Hashmap', tech: 'Python3', link: 'https://github.com/hgjohn/Hashmap', image: 'images/hashmap.png'}
 ];
 
