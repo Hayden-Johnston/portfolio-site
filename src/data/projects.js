e1d87491213f7f03b6c6b739344d5b8ca70b8d00
Hayden-Johnston <hjohnston.g@gmail.com>
2023-08-23 01:17:52 -0700

    added projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,11 @@
 
 // Define a JS object with arrays of projects, their tech stack, link to github, and image
 const projects = [
-    { project: 'Hashmap', tech: 'Python3', link: 'https://github.com/hgjohn/Hashmap', image: 'images/hashmap.png'},
+    { project: 'MERN Inventory', tech: 'React, Node.js, Express, MongoDB', link: 'https://github.com/Hayden-Johnston/MERN-inventory', image: 'images/mern-inventory.jpg'},
+    { project: 'Historical Stock Analysis', tech: 'Python, Yfinance, BeautifulSoup, Pandas, Plotly', link: 'https://github.com/Hayden-Johnston/IBM-datascience', image: 'images/stock-analysis.jpg'},
+    { project: 'Discord Chatbot', tech: 'Python, Discord.py, OpenAI API, Docker', link: 'https://github.com/hgjohn/Discord-GPT-bot', image: 'images/QR8R.jpg'},
+    { project: 'Flask Backend', tech: 'Python, Flask, SQlite', link: 'https://github.com/Hayden-Johnston/Flask-SQLite-backend', image: ''},
+    { project: 'Portfolio Site', tech: 'React, Node.js, MaterialUI', link: 'https://github.com/Hayden-Johnston/portfolio-site', image: ''},
     { project: 'Hashmap', tech: 'Python3', link: 'https://github.com/hgjohn/Hashmap', image: 'images/hashmap.png'}
 ];
 
