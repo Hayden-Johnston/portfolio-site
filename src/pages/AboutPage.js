73e5115033dc99ef6eac7e7efff68a18e25697ac
Hayden-Johnston <hjohnston.g@gmail.com>
2023-08-23 01:17:59 -0700

    changed info, added image

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,12 +4,33 @@ function AboutPage() {
     return (
         <>
 
-    <h2>This is the about page</h2>
-    <article>
-        <h3></h3>
-        <p>
-        </p>
-    </article>
+    <div class="container">
+        <img src="images/selfie.png" class="portrait"></img>
+        <article>
+            <h2>I'm Hayden</h2>
+            <p>
+                Initially trained in biochemistry, I'm currently studying computer science to pursue software engineering. I have interest in full stack development, data science, and machine learning.
+                I'm especially interested in the intersection of biology and computer science, and I'm looking to work on projects that combine the two.
+                I am also intrigued by devops practices such as CI/CD and IaC.  <br></br><br></br>
+                I run a homelab server that hosts a variety of services including a dev environment, a NextCloud Instance, and Authelia.
+                If I'm not working on my server, I'm playing chess or Baldur's Gate 3.
+                <br></br>
+                <br></br>
+                I have experience with:
+                <ul>
+                    <li>Python</li>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                    <li>Node.js</li>
+                    <li>Flask</li>
+                    <li>SQL</li>
+                    <li>Git</li>
+                    <li>Linux</li>
+                    <li>Docker</li>
+                </ul>
+            </p>
+        </article>
+    </div>
         </>
     );
 }
