dcf217c7c829b59c7fbb174ae25e9f463954867b
Hayden-Johnston <hjohnston.g@gmail.com>
2023-08-23 18:38:31 -0700

    changed input field size

--- src/components/TextInputs.js
+++ src/components/TextInputs.js
@@ -2,12 +2,15 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
+
 export default function TextInputs() {
+
   return (
+
     <Box
       component="form"
       sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
+        '& .MuiTextField-root': { m: 1, width: '60ch' },
       }}
       noValidate
       autoComplete="off"
@@ -15,25 +18,32 @@ export default function TextInputs() {
       <div>
         <TextField
           required
-          id="outlined-required"
+          id="standard-required"
           label="Name"
+          name="from_name"
+          width="100%"
         />
         <br></br>
         <TextField
           required
-          id="outlined-required"
+          id="standard-required"
           label="Email"
+          name="user_email"
         />
         <br></br>
         <TextField
-          id="outlined-required"
+          id="standard-required"
           label="Subject"
+          name="subject"
         />
         <br></br>
         <TextField
           required
-          id="outlined-required"
+          id="standard-required"
           label="Message"
+          name="message"
+          multiline
+          rows={5}
         />
         
       </div>
