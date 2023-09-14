6b87e88e42db39e478a874da7d02146653ebd3de
Hayden-Johnston <hjohnston.g@gmail.com>
2023-09-14 01:12:23 -0700

    spellcheck

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -12,13 +12,13 @@ function AboutPage() {
             <br></br>
             <Button download>Download Resume</Button>
             <p>
-                Initially trained in biochemistry, I'm currently studying computer science to pursue software engineering. I have interest in full stack development, data science, and dev ops.
+                Initially trained in biochemistry, I'm currently studying computer science to pursue software engineering. I have an interest in full-stack development, data science, and dev ops.
                 This site is continuously deployed on Netlify and I am exploring building robust CI/CD pipelines.
-                I am also studying for the AWS Solutions Architect certification and learning to applying Terraform as an IaC solution.
+                I am also studying for the AWS Solutions Architect certification and learning to apply Terraform as an IaC solution.
                 <br></br><br></br>
                 
-                I run a homelab server that I use to experiment with different Linux environments, run self hosted services, and learn dev tools.
-                I am in the process of configuring GPU nodes that I intend on leveraging for machine learning projects.  I am also currently using it to learn Kubernetes on a local cluster running CentOS.
+                I run a home lab server that I use to experiment with different Linux environments, run self-hosted services, and learn dev tools.
+                I am in the process of configuring GPU nodes that I intend to leverage for machine learning projects.  I am also currently using it to learn Kubernetes on a local cluster running CentOS.
                 Outside of the tech world I enjoy games and travelling.
                 <br></br>
                 <br></br>
