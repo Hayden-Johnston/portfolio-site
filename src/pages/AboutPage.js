9397b30ea7fca2df194bf5fca5b50cec3186b11f
Hayden-Johnston <hjohnston.g@gmail.com>
2023-09-13 22:31:58 -0700

    update about me

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -15,8 +15,9 @@ function AboutPage() {
                 Initially trained in biochemistry, I'm currently studying computer science to pursue software engineering. I have interest in full stack development, data science, and machine learning.
                 I'm especially interested in the intersection of biology and computer science, and I'm looking to work on projects that combine the two.
                 I am also intrigued by dev ops practices such as CI/CD and implementing infrastructure as code.  <br></br><br></br>
+                
                 I run a homelab server that I use to experiment with different Linux environments, run self hosted services, and learn dev tools.
-                I am currently using it to learn Kubernetes on a local cluster running CentOS.  It also contains a GPU cluster that I intend on leveraging for machine learning projects.
+                I am in the process of configuring GPU nodes that I intend on leveraging for machine learning projects.  I am also currently using it to learn Kubernetes on a local cluster running CentOS.  
                 Outside of the tech world I enjoy games and travelling.
                 <br></br>
                 <br></br>
