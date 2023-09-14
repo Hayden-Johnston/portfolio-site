ff78b88112b58984e8cf47371f7b85035ad2c473
Hayden-Johnston <hjohnston.g@gmail.com>
2023-09-13 17:45:40 -0700

    updated about me

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -14,8 +14,9 @@ function AboutPage() {
             <p>
                 Initially trained in biochemistry, I'm currently studying computer science to pursue software engineering. I have interest in full stack development, data science, and machine learning.
                 I'm especially interested in the intersection of biology and computer science, and I'm looking to work on projects that combine the two.
-                I am also intrigued by devops practices such as CI/CD and IaC.  <br></br><br></br>
-                I run a homelab server that hosts a variety of services including authentication, a dev environment, and a NextCloud Instance.  It allows me to experiment with different linux environments and deploy containers which I route through DDNS and a reverse proxy.
+                I am also intrigued by dev ops practices such as CI/CD and implementing infrastructure as code.  <br></br><br></br>
+                I run a homelab server with a GPU cluster to accelerate ML projects.  It also allows me to experiment with different linux environments and deploy self hosted services.
+                I am currently using it to learn Kubernetes before I move on to Terraform.
                 Outside of the tech world I enjoy games and travelling.
                 <br></br>
                 <br></br>
@@ -23,13 +24,13 @@ function AboutPage() {
                 <ul class="two-column-list">
                     <li>Python</li>
                     <li>JavaScript</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
                     <li>React</li>
                     <li>Node.js</li>
                     <li>Flask</li>
-                    <li>SQL</li>
-                    <li>MongoDB</li>
+                    <li>Databases</li>
                     <li>Git</li>
-                    <li>Linux</li>
                     <li>Docker</li>
                 </ul>
             </p>
