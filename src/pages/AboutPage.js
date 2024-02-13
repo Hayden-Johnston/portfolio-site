02f7c31d35589d06de72a9c934e513c8fd4b9426
Hayden-Johnston <hjohnston.g@gmail.com>
2024-02-13 02:05:24 -0800

    updated about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -6,20 +6,18 @@ function AboutPage() {
         <>
 
     <div class="center-container" style={{ paddingTop:"40px" }}>
-        <img src="images/selfie.png" alt="" class="portrait" width="250" margin="20px"></img>
+        {/* <img src="images/selfie.png" alt="" class="portrait" width="250" margin="20px"></img> */}
         <article class="container">
             <h2>I'm Hayden, a US based software developer.</h2>
             <br></br>
             <Button download>Download Resume</Button>
             <p>
                 Initially trained in biochemistry, I'm currently studying computer science to pursue software engineering. I have an interest in full-stack development, data science, and dev ops.
-                I am actively studying for the AWS Solutions Architect Associate certification to learn about cloud computing and infrastructure.  In the future I plan to explore Terraform as an IaC solution.
-                I am also learning about implementing robust CI/CD pipelines and have set this site up with continuous deployment through Netlify.
                 <br></br><br></br>
                 
                 I run a home lab server that I use to experiment with different Linux environments, deploy self-hosted services, and learn dev tools.
-                I am in the process of configuring GPU nodes that I intend to leverage for machine learning projects.  I am also currently learning Kubernetes with a local cluster running on CentOS.
-                Outside of the tech world I enjoy games and travelling.
+                I am looking to expand my knowledge and skills in cloud computing, IaC, and CI/CD.
+                Outside of the tech world I enjoy strategy games and travelling.
                 <br></br>
                 <br></br>
                 I have experience with:
